c9a4d7f792c1edba95daf94f8c38fe0ce81fcea2
ヨン ソジョン <yeonsojeong@yonuiMaBookpuro.elecom>
2020-09-28 19:19:38 +0900

    Web app Yeon sojeong

---
+++ script.js
@@ -0,0 +1,112 @@
+$('#quiz').quiz({
+  //resultsScreen: '#results-screen',
+  counter: false,
+  //homeButton: '#custom-home',
+  
+  questions: [
+    {
+      'q': 'ネコはどんなきもち？'
+      +'<img src="img/neko_heion.png">',
+      'options': [
+        'おちついている',
+        'こわがっている'
+      ],
+      'correctIndex': 0,
+      'correctResponse': 'よくやった！'
+      +'<img src="img/maru.png">',
+      'incorrectResponse': 'おしいね,,'
+      +'<img src="img/batu.png">',
+    },
+    {
+      'q': 'ネコはどんなきもち？'
+      +'<img src="img/neko_okoru.png">',
+      'options': [
+        'よろこんでいる',
+        'おこっている',
+      ],
+      'correctIndex': 1,
+      'correctResponse': 'よくやった！'
+      +'<img src="img/maru.png">',
+      'incorrectResponse': 'おしいね、、'
+      +'<img src="img/batu.png">',
+    },
+    {
+      'q': 'ネコはどんなきもち？'
+      +'<img src="img/neko_keikai.png">',
+      'options': [
+        'けいかいしている',
+        'イライラしている'
+      ],
+      'correctIndex': 0,
+      'correctResponse': 'よくやった！'
+      +'<img src="img/maru.png">',
+      'incorrectResponse': 'おしいね、、'
+      +'<img src="img/batu.png">',
+    },
+    {
+      'q': 'ネコはどんなきもち？'
+      +'<img src="img/neko_uresii.png">',
+      'options': [
+        'かなしんでいる',
+        'うれしがっている',
+      ],
+      'correctIndex': 1,
+      'correctResponse': 'よくやった！'
+      +'<img src="img/maru.png">',
+      'incorrectResponse': 'おしいね、、'
+      +'<img src="img/batu.png">',
+    },
+    {
+      'q': 'ネコはどんなきもち？'
+      +'<img src="img/neko_suki.png">',
+      'options': [
+        'すきすきすきすき',
+        'イライラしている',
+      ],
+      'correctIndex': 0,
+      'correctResponse': 'よくやった！'
+      +'<img src="img/maru.png">',
+      'incorrectResponse': 'おしいね、、'
+      +'<img src="img/batu.png">',
+    },
+    {
+      'q': 'ネコのきもちは？'
+      +'<img src="img/neko_kowai.png">',
+      'options': [
+        'うれしがっている',
+        'こわがっている',
+      ],
+      'correctIndex': 1,
+      'correctResponse': 'よくやった！'
+      +'<img src="img/maru.png">',
+      'incorrectResponse': 'おしいね、、'
+      +'<img src="img/batu.png">',
+    },
+    {
+      'q': 'ネコのきもちは？'
+      +'<img src="img/neko_iraira.png">',
+      'options': [
+        'おちついている',
+        'イライラしている',
+      ],
+      'correctIndex': 1,
+      'correctResponse': 'よくやった！'
+      +'<img src="img/maru.png">',
+      'incorrectResponse': 'おしいね、、'
+      +'<img src="img/batu.png">',
+    },
+      {
+      'q': 'ネコのきもちは？'
+      +'<img src="img/neko_kininaru.png">',
+      'options': [
+        '気になっている',
+        'かなしんでいる',
+      ],
+      'correctIndex': 0,
+      'correctResponse': 'よくやった！'
+      +'<img src="img/maru.png">',
+      'incorrectResponse': 'おしいね、、'
+      +'<img src="img/batu.png">',
+    }
+  ]
+});
